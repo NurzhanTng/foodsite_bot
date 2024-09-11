690fa274025f3f9aa8554f3d74751c1094bfc8ad
Tangatarov Nurzhan <tangatarovnurzan2@gmail.com>
2024-09-11 12:12:34 +0500

    test

--- core/handlers/manager/mailing.py
+++ core/handlers/manager/mailing.py
@@ -28,23 +28,26 @@ async def send_mailing(message: Message, bot: Bot, chat_handler: ChatHistoryHand
         return
 
     users = await rest.post('service/users_find/', data={"role": "admin"})
-    needed_ids = ["1618183152","878852186","804430069","855106661","452075811","742501371","734952244","7083460631","5472448283","6497941056","5874139243","333591649","5555863688","5052456121","929957030","744215752","1375037247","5106374444","195456138","532300099","771033809","667278926","792874867","1615879151","1120989491","6771759035","1493758385","5316566417","546121705","938644744","6002574127","538232087"]
+    needed_ids = ["1618183152", "878852186", "804430069", "855106661", "452075811", "742501371", "734952244",
+                  "7083460631", "5472448283", "6497941056", "5874139243", "333591649", "5555863688", "5052456121",
+                  "929957030", "744215752", "1375037247", "5106374444", "195456138", "532300099", "771033809",
+                  "667278926", "792874867", "1615879151", "1120989491", "6771759035", "1493758385", "5316566417",
+                  "546121705", "938644744", "6002574127", "538232087"]
     await message.answer(f'Рассылка началась. Количество пользователей для рассылки: {len(users)}')
     error_users = {}
     for user in users:
         chat_id = user.get('telegram_id', -1)
         try:
             message_id = (await bot.send_photo(chat_id,
-                                               "https://pizza.pizzeria-almaty.kz/media/images/margarita_low.jpg")
+                                               "https://ucarecdn.com/6d35201e-acc6-46e9-b13c-c53d3d0d170d/-/format/auto/-/preview/3000x3000/-/quality/lighter/pf-dfc945db--50sale.png")
                           ).message_id
             chat_handler.add_new_message(chat_id, message_id)
             message_id = (await bot.send_message(chat_id,
                                                  "Скидка 50% на доставку! \n\nУспейте воспользоваться нашим выгодным "
                                                  "предложением! Получите 50% скидку на доставку всех блюд при заказе "
-                                                 "через наше онлайн-кафе. Просто оформите заказ через нашего бота, "
+                                                 "через нашу онлайн пиццерию. Просто оформите заказ через нашего бота, "
                                                  "и скидка будет применена автоматически. Наслаждайтесь любимыми "
-                                                 "блюдами с удобной доставкой прямо к вам домой! \n\nАкция действует "
-                                                 "до конца месяца. Не упустите шанс!",
+                                                 "блюдами с удобной доставкой прямо к вам домой!",
                                                  reply_markup=get_mailing_inline_keyboard())).message_id
             chat_handler.add_new_message(chat_id, message_id)
         except Exception as e:
@@ -65,4 +68,3 @@ async def go_back(callback: CallbackQuery, chat_handler: ChatHistoryHandler, res
     await callback.bot.send_message(1234249296, f"{callback.message.chat.id} отклонил акцию")
     await chat_handler.delete_messages(callback.message.chat.id)
     await get_start(callback.message, chat_handler, rest, state, command)
-
